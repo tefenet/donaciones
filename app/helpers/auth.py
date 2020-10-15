ac0f2d04e14a85a3c3e59d0494e3a1e416e6f1e1
ucuraj <ulisescurajauregui@gmail.com>
2020-10-15 20:33:07 -0300

    add helpers for authentication and admin check

--- app/helpers/auth.py
+++ app/helpers/auth.py
@@ -1,2 +1,39 @@
+from flask import session, url_for, flash, redirect, abort, render_template
+from functools import wraps
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if authenticated(session):
+            return f(*args, **kwargs)
+        else:
+            flash("Tenes estar logueado para acceder a esta página!")
+            return redirect(url_for('login'))  # url_login
+
+    return wrap
+
+
+def admin_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if administrator(session):
+            return f(*args, **kwargs)
+        else:
+            abort(401)
+
+    return wrap
+
+
 def authenticated(session):
-    return session.get("user")
+    return session.get('logged_in')
+
+
+def administrator(session):
+    if ('user' in session) and (session['user'].tipo_cuenta == 1):
+        return True
+    return False
+
+
+def clear_session(session):
+    [session.pop(key) for key in list(session.keys()) if key != '_flashes']
