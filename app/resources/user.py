aa8b52c8ebae70cf091a58ed8e7b1855650b80a9
ucuraj <ulisescurajauregui@gmail.com>
2020-10-15 20:34:44 -0300

    updated create, deactivate

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,31 +1,47 @@
-from flask import redirect, render_template, request, url_for, session, abort
+from flask import redirect, render_template, request, url_for, session, abort, flash
 from app.db import dbSession
 from app.models.user import User
-from app.helpers.auth import authenticated
+from app.helpers.auth import authenticated, admin_required
+from app.helpers.handler import display_errors
+from app.resources.forms import CreateUserForm
+from pymysql import escape_string as thwart
 
 
 # Protected resources
+@admin_required
 def index():
-    if not authenticated(session):
-        abort(401)
-
     users = User.query.all()
     return render_template("user/index.html", users=users)
 
 
+@admin_required
 def new():
-    if not authenticated(session):
-        abort(401)
-
-    return render_template("user/new.html")
+    form = CreateUserForm()
+    return render_template("user/new.html", form=form)
 
 
+@admin_required
 def create():
-    if not authenticated(session):
-        abort(401)
-
-    args = list(request.form.values())
-    user = User(email=args[0], password=args[1], first_name=args[2], last_name=args[3])
+    """ Da de alta un usuario en la base de datos."""
+    form = CreateUserForm(request.form)
+    user = User(email=thwart(form.email.data), username=thwart(form.username.data),
+                first_name=thwart(form.first_name.data), last_name=thwart(form.last_name.data), active=form.active.data)
+    user.set_password(thwart(form.password.data))  # envio la pw para guardar el hash en la db.
     dbSession.add(user)
     dbSession.commit()
-    return redirect(url_for("user_index"))
+    if form.errors:
+        display_errors(form.errors)  # si hay errores redirecciona a la pagina de crear usuario y muestra los errores.
+        return url_for("new_user")
+
+    return redirect(url_for("index"))
+
+
+def deactive_account(id=None):
+    """Recibe un id de usuario. Si el usuario existe desactiva la cuenta seteando el campo active a False."""
+    user = User.query.get(id=id)
+    if user and (user.active is True):
+        user.active = False
+        dbSession.commit()
+        flash("El usuario {} fue desactivado exitosamente".format(user.email), "success")
+    flash("Usuario con id {} no encontrado".format(id), "danger")
+    return redirect(url_for("deactivate_account"))
