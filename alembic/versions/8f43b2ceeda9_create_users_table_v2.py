3cdfc25f02153d9db8b412fe95f042520d6542fc
Braian Blanco <brai_ladoce@hotmail.com>
2020-10-16 18:16:44 -0300

    seeds de usuarios de prueba con alembic

--- alembic/versions/8f43b2ceeda9_create_users_table_v2.py
+++ alembic/versions/8f43b2ceeda9_create_users_table_v2.py
@@ -28,7 +28,7 @@ def upgrade():
     account_type = Column(SmallInteger)
     create_date = Column(DateTime, default=datetime.now())"""
 
-    op.create_table(
+    users_table = op.create_table(
         'users',
         sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
         sa.Column('email', sa.String(100), nullable=False, unique=True, index=True),
@@ -40,6 +40,18 @@ def upgrade():
         sa.Column('account_type', sa.SmallInteger(), default=2),
         sa.Column('create_date', sa.DateTime, default=datetime.now())
     )
+
+    #seed de usuarios de prueba:
+
+    op.bulk_insert(users_table,
+    [
+        {'id':1, 'username':'admin', 'password_hash':'pbkdf2:sha256:150000$QhWXVmaA$2028ef705c136b76d37f8e306cb8f4fab11fc63a58a8dd7ed6ea150a385a4579', 
+        'first_name':'Admin', 'last_name':'Del Sitio', 'email':'admin@donaciones.gov', 'active':True, 'account_type': 1, 'create_date': '2020-10-16 01:03:03'},
+        {'id':2, 'username':'operador', 'password_hash':'pbkdf2:sha256:150000$QhWXVmaA$2028ef705c136b76d37f8e306cb8f4fab11fc63a58a8dd7ed6ea150a385a4579', 
+        'first_name':'Operador', 'last_name':'Del Centro', 'email':'operadorUNICEF@gmail.com', 'active':True, 'account_type': 2, 'create_date': '2020-10-16 01:03:03'},
+    ]
+)
+
 
 
 def downgrade():
