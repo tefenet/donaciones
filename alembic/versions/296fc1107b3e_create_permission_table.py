ded5807c806f4c18444e110cb5f70483c687f88f
Braian Blanco <brai_ladoce@hotmail.com>
2020-10-16 17:49:53 -0300

    terminadas las tablas de roles y permisos

--- alembic/versions/296fc1107b3e_create_permission_table.py
+++ alembic/versions/296fc1107b3e_create_permission_table.py
@@ -17,12 +17,34 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
+    permission_table = op.create_table(
         'permission',
         sa.Column('id', sa.Integer, primary_key=True),
         sa.Column('name', sa.String(50), nullable=False),
     )
 
+    op.bulk_insert(permission_table,
+    [
+        {'id':1, 'name':'user_index'},
+        {'id':2, 'name':'user_new'},
+        {'id':3, 'name':'user_destroy'},
+        {'id':4, 'name':'user_update'},
+        {'id':5, 'name':'user_find'},
+        {'id':6, 'name':'user_show'},
+        {'id':7, 'name':'role_index'},
+        {'id':8, 'name':'role_new'},
+        {'id':9, 'name':'role_show'},
+        {'id':10, 'name':'role_destroy'},
+        {'id':11, 'name':'role_update'},
+        {'id':12, 'name':'permission_index'},
+        {'id':13, 'name':'permission_show'},
+        {'id':14, 'name':'permission_new'},
+        {'id':15, 'name':'permission_destroy'},
+        {'id':16, 'name':'permission_update'},
+        {'id':17, 'name':'system_modify_config'},
+        {'id':18, 'name':'user_admin_login_logout'},
+    ]
+ )
 
 def downgrade():
     op.drop_table('permission')
--- alembic/versions/909182962dcb_create_role_has_permission_table.py
+++ alembic/versions/909182962dcb_create_role_has_permission_table.py
@@ -1,4 +1,4 @@
-"""create role has permission table
+"""create role has permission table#permission_index
 
 Revision ID: 909182962dcb
 Revises: 296fc1107b3e
@@ -17,12 +17,35 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
+    role_has_permission_table = op.create_table(
         'role_has_permission',
         sa.Column('id', sa.Integer, primary_key=True),
         sa.Column('role_id', sa.Integer, nullable=False),
         sa.Column('permission_id', sa.Integer, nullable=False),
     )
+
+    op.bulk_insert(role_has_permission_table,
+    [
+        {'role_id':1, 'permission_id':1}, #user_index
+        {'role_id':1, 'permission_id':2}, #user_show
+        {'role_id':1, 'permission_id':3}, #user_destroy
+        {'role_id':1, 'permission_id':4}, #user_update
+        {'role_id':1, 'permission_id':5}, #user_find
+        {'role_id':1, 'permission_id':6}, #user_show
+        {'role_id':1, 'permission_id':7}, #role_index
+        {'role_id':1, 'permission_id':8}, #role_new
+        {'role_id':1, 'permission_id':9}, #role_show
+        {'role_id':1, 'permission_id':10}, #role_destroy
+        {'role_id':1, 'permission_id':11}, #role_update
+        {'role_id':1, 'permission_id':12}, #permission_index
+        {'role_id':1, 'permission_id':13}, #permission_show
+        {'role_id':1, 'permission_id':14}, #permission_new
+        {'role_id':1, 'permission_id':15}, #permission_destroy
+        {'role_id':1, 'permission_id':16}, #permission_update
+        {'role_id':1, 'permission_id':17}, #system_modify_config
+        {'role_id':1, 'permission_id':18}, #user_admin_login_logout
+    ])
+
 
 
 def downgrade():
