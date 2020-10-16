b2ea6b36cb2a70bc2d3cb45d2b10a473045c07b0
Braian Blanco <brai_ladoce@hotmail.com>
2020-10-16 07:20:24 -0300

    migracion tablas rol_tiene_permiso & user_tiene_rol

---
+++ alembic/versions/909182962dcb_create_role_has_permission_table.py
@@ -0,0 +1,30 @@
+"""create role has permission table
+
+Revision ID: 909182962dcb
+Revises: 296fc1107b3e
+Create Date: 2020-10-16 07:11:09.293161
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '909182962dcb'
+down_revision = '296fc1107b3e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'role_has_permission',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('role_id', sa.Integer, nullable=False),
+        sa.Column('permission_id', sa.Integer, nullable=False),
+    )
+
+
+def downgrade():
+    def downgrade():
+    	op.drop_table('role_has_permission')
---
+++ alembic/versions/c80280ba772c_create_user_has_role_table.py
@@ -0,0 +1,30 @@
+"""create user has role table
+
+Revision ID: c80280ba772c
+Revises: 909182962dcb
+Create Date: 2020-10-16 07:11:30.591623
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c80280ba772c'
+down_revision = '909182962dcb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'user_has_role',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('user_id', sa.Integer, nullable=False),
+        sa.Column('role_id', sa.Integer, nullable=False),
+    )
+
+
+def downgrade():
+    def downgrade():
+    	op.drop_table('user_has_role')
