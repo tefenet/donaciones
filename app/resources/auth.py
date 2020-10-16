11be4d3bec7595561d16c17295f42c87d49cf9fd
ucuraj <ulisescurajauregui@gmail.com>
2020-10-16 02:11:39 -0300

    updated authenticate

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,5 +1,6 @@
 from flask import redirect, render_template, request, url_for, session, flash, current_app as app
 from app.helpers.auth import authenticated, login_required, clear_session
+from app.helpers.handler import display_errors
 from app.resources.forms import LoginForm
 from app.models.user import User
 from pymysql import escape_string as thwart
@@ -29,36 +30,41 @@ def authenticate():
     La funcion thwart del paquete pymsql se encarga de sanitizar el parametro para evitar posibles sql injections"""
 
     form = LoginForm(request.form)
+    if form.validate():
+        username = thwart(form.username.data.lower())
+        password = thwart(form.password.data.lower())
 
-    username = thwart(form.username.data.lower())
-    password = thwart(form.password.data.lower())
+        app.logger.info("username {}".format(username))
 
-    app.logger.info("username {}".format(username))
-
-    user = User.query.filter(
-        User.email == username).first()  # primero compruebo el que exista el correo, sino voy por el nombre de usuario
-    if user is not None:
-        if user.check_password(password) is False:  # pregunto si la pw conincide con el hash almacenado
-            flash("Usuario/Email o Clave incorrecto.", "danger")
-            return redirect(url_for('auth_login'))
-    else:
-        # si llegué aca es porque no encontro el mail, pruebo buscando el username
-        user = User.query.filter(User.username == username).first()
-        if user is None:
-            flash("Usuario/Email o Clave incorrecto.", "danger")
-            return redirect(url_for('auth_login'))
-        else:
-            if user.check_password(password) is False:
+        user = User.query.filter(
+            User.email == username).first()  # primero compruebo el que exista el correo, sino voy por el nombre de usuario
+        if user is not None:
+            if user.check_password(password) is False:  # pregunto si la pw conincide con el hash almacenado
                 flash("Usuario/Email o Clave incorrecto.", "danger")
                 return redirect(url_for('auth_login'))
+        else:
+            # si llegué aca es porque no encontro el mail, pruebo buscando el username
+            user = User.query.filter(User.username == username).first()
+            if user is None:
+                flash("Usuario/Email o Clave incorrecto.", "danger")
+                return redirect(url_for('auth_login'))
+            else:
+                if user.check_password(password) is False:
+                    flash("Usuario/Email o Clave incorrecto.", "danger")
+                    return redirect(url_for('auth_login'))
+
         if not user.active:
-            flash("La cuenta que has ingresado está desactivada.", "danger")
+            flash("La cuenta que has ingresado se encuentra inactiva.", "danger")
             return redirect(url_for('auth_login'))
 
-    app.logger.info("user: %s", user)
-    set_session(user)
-    flash("La sesión se inició correctamente.", "success")
-    return redirect(url_for("home"))
+        app.logger.info("user: %s", user)
+        set_session(user)
+        flash("La sesión se inició correctamente.", "success")
+        return redirect(url_for("home"))
+
+    if form.errors:
+        display_errors(form.errors)
+        return redirect(url_for("auth_login"))
 
 
 @login_required
