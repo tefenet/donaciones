115dcb60a05e17759eacca494b3fcbcbc29949e5
ucuraj <ulisescurajauregui@gmail.com>
2020-11-10 11:39:59 -0300

    actualizado seed turnos

--- alembic/versions/bb13f2d0ce00_create_shifts_table.py
+++ alembic/versions/bb13f2d0ce00_create_shifts_table.py
@@ -102,6 +102,18 @@ def upgrade():
 
     op.bulk_insert(shifts_table, turnos)
 
+    turnos = []
+    hora_inicio = time(9, 00)
+    hora_fin = time(9, 30)
+    for i in range(45, 59):
+        turno = {
+            'id': i, 'donor_email': 'donor{}@test.com'.format(i), 'donor_phone': '+54 555 555-5555',
+            'start_time': hora_inicio, 'end_time': hora_fin, 'date': date.today(), 'center_id': 7
+        }
+        hora_inicio = increment_time(hora_inicio)
+        hora_fin = increment_time(hora_fin)
+        turnos.append(turno)
+    op.bulk_insert(shifts_table, turnos)
 
 def downgrade():
     op.drop_table('shifts')
