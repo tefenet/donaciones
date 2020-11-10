3051d600d6d80cc77f6ced4bf39f5c276526caa5
ucuraj <ulisescurajauregui@gmail.com>
2020-11-10 19:47:20 -0300

    fix datetime converters

--- app/helpers/formatter.py
+++ app/helpers/formatter.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 
-def str_time_to_datetime(cls, str_time):
+def str_time_to_datetime(str_time):
     return datetime.strptime(str_time, '%H:%M')
 
 
-def str_date_to_datetime(cls, str_date):
+def str_date_to_datetime(str_date):
     return datetime.strptime(str_date, '%Y-%m-%d')
