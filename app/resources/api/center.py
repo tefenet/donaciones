13cddf49836d3ec42d13ca626e23fb54b6cc75ad
Braian Blanco <brai_ladoce@hotmail.com>
2020-11-13 08:44:03 -0300

    API center create

--- app/resources/api/center.py
+++ app/resources/api/center.py
@@ -1,5 +1,7 @@
-from flask import jsonify
+from flask import jsonify, request
+from app import dbSession
 from app.models.center import Center
+from app.models.sistema import Sistema
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -28,10 +30,32 @@ def show(center_id):
 
 def index():
 	"""retorna en formato JSON los datos de todos los centros de ayuda publicados en el sitio"""
+	cant_pagina = Sistema.get_sistema().cant_por_pagina
 	#paginar segun la variable del sistema
 	return "ruta de centro index (GET)"
 
 
 def create():
 	"""crea un nuevo centro de ayuda"""
-	return "ruta de centro create (POST)"
+	try:
+		data = request.get_json()
+		# si el mimetype no indica JSON retornara None
+		if data is None:
+			return jsonify({'error': [
+				{'status': 'Bad Request', 'error_msg': 'Solicitud no valida', 'error_code': 400}]}), 400
+		new_center = Center()
+		new_center.name = data['nombre']
+		new_center.address = data['direccion']
+		new_center.phone = data['telefono']
+		new_center.opening = data['hora_apertura']
+		new_center.closing = data['hora_cierre']
+		new_center.type = data['tipo']
+		new_center.web_site = data['web']
+		new_center.email = data['email']
+		dbSession.add(new_center)
+		dbSession.commit()
+		return "Nuevo centro creado con exito!", 201 #retornar tambien el centro en formato JSON
+	except:
+		return jsonify(error_code=500, error_msg="Error inesperado", status="internal server error"), 500
+
+
