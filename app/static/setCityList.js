256e5e3eb4e9d1fb37a68d9e23ee3304addd055b
pablo kepes <pablokepes@gmail.com>
2020-11-15 19:45:05 -0300

    fix update 3

--- app/static/setCityList.js
+++ app/static/setCityList.js
@@ -1,33 +1,34 @@
 let cityList = []
-
 function getAjax(page) {
-    return $.get({
-        url: 'https://api-referencias.proyecto2020.linti.unlp.edu.ar/municipios?page=' + page,
-    })
-  }  
-  function setOptions (jsonResponse) {
-      let towns= jsonResponse.data.Town          
-          for (t in towns){
-            cityList.push(towns[t])
-            }
-          let page = jsonResponse.page
-          if ((jsonResponse.total - page * jsonResponse.per_page) > 0){
-            getAjax(page+1).done(setOptions)
-          }
-          else{            
-            select = document.getElementById('city_id')
-            while (select.childNodes.length >= 1) {
-                            select.removeChild(select.firstChild);
-                        }
-            function appendOption (city) {
-              newOption = document.createElement('option');
-              newOption.value = city.id;
-              newOption.text = city.name;
-              select.appendChild(newOption);              
-            }            
-            cityList.sort((a,b)=> a.name > b.name).forEach(appendOption)    
-          }
+  return $.get({
+    url: 'https://api-referencias.proyecto2020.linti.unlp.edu.ar/municipios?page=' + page,
+  })
+}
+function setOptions(jsonResponse) {
+  let towns = jsonResponse.data.Town
+  for (t in towns) {
+    cityList.push(towns[t])
   }
-  window.onload = () => {
-    getAjax(1).done(setOptions)    
+  let page = jsonResponse.page
+  if ((jsonResponse.total - page * jsonResponse.per_page) > 0) {
+    getAjax(page + 1).done(setOptions)
   }
+  else {
+    let select = document.getElementById('city_id')
+    const selected = select.selectedOptions[0].value
+    while (select.childNodes.length >= 1) {
+      select.removeChild(select.firstChild);
+    }
+    function appendOption(city) {
+      newOption = document.createElement('option');
+      newOption.value = city.id;
+      newOption.text = city.name;
+      select.appendChild(newOption);
+    }
+    cityList.sort((a, b) => a.name > b.name).forEach(appendOption)
+    select.value = selected ? selected : 1
+  }
+}
+window.onload = () => {
+  getAjax(1).done(setOptions)
+}
