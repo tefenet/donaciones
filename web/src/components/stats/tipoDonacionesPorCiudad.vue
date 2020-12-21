b54bcde8eea4b77a8a01722c3c7a374c9272b9e5
Braian Blanco <brai_ladoce@hotmail.com>
2020-12-20 22:50:24 -0300

    grafico: tipo de donaciones por ciudad

--- web/src/components/stats/tipoDonacionesPorCiudad.vue
+++ web/src/components/stats/tipoDonacionesPorCiudad.vue
@@ -3,12 +3,13 @@
   <div>
     <h1>Estadisticas</h1>
     <p>Tipos de Donaciones por ciudad</p>
-    <div v-if="ciudadSinDonaciones">
-      <span>No tenemos registros de donaciones en centros de esta ciudad :( </span>
-    </div>
     <b-container style="max-width: 450px">
-     <b-form-select id="input-ciudad" v-model="ciudad" :options="ciudades" size="sm"> </b-form-select>
+      <span> Selecciona una ciudad </span>
+    <b-form-select id="input-ciudad" v-model="ciudad_id" :options="ciudades" @change="selectCity()" size="sm"> </b-form-select>
     </b-container>
+    <div v-if="ciudadSinDonaciones"><br>
+      <h5>No tenemos registros de donaciones en centros de esta ciudad :( </h5>
+    </div>
     <b-container>
       <ve-pie :data="chartData"></ve-pie>
     </b-container>
@@ -18,7 +19,7 @@
 <script>
 import VePie from 'v-charts/lib/pie.common'
 import axios from "axios";
-//import {API_LOCATION} from "@/config";
+import {API_LOCATION} from "@/config";
 import {API_REF_LOCATION} from "@/config";
 
 export default {
@@ -26,15 +27,13 @@ export default {
   components: {VePie},
   data() {
     return {
-      ciudad: {},
+      ciudadStats: [],
+      ciudad_id: [],
       ciudades: [],
       ciudadSinDonaciones: false, //posiblemente convenga que sea una propiedad computada
       chartData: {
-        columns: ['tipo', 'cantidad'],
+        columns: ['tipo', 'donaciones'],
         rows: [
-          {'tipo': 'alimentos', cantidad: 2},
-          {'tipo': 'salud', cantidad: 5},
-          {'tipo': 'general', cantidad: 4},
         ]
       }
     }
@@ -44,6 +43,27 @@ export default {
       return Object.values(ciudadesResponse).map(function (c) {
         return {'value': c.id, 'text': c.name};
       });
+    },
+    cityIsEmpty() {
+      return Object.keys(this.ciudadStats).length === 0;
+    },
+    updateStats() {
+      this.chartData.rows = [];
+      Object.keys(this.ciudadStats).forEach(key => {
+        this.chartData.rows.push({'tipo': key , 'donaciones': this.ciudadStats[key] });
+      });
+    },
+    selectCity(){
+      axios
+        .get(API_LOCATION + 'stats/byCity/' + this.ciudad_id)
+        .then(response => {
+        this.ciudadStats = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+        this.ciudadSinDonaciones = this.cityIsEmpty();
+        this.updateStats();
     },
   },
   mounted() {
